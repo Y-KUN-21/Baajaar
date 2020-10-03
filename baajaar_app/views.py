36cc4094b9700d920d8ce63182cc1dcaf55efb41
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-10-03 21:11:15 +0530

    Add files via upload

--- baajaar_app/views.py
+++ baajaar_app/views.py
@@ -3,12 +3,24 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Customer, Product, Order, OrderedItem
+
+
 # Create your views here.
 
 
 def home(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, completed=False)
+        items = order.ordereditem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'baajaar_app/home.html', context)
 
 
@@ -33,6 +45,20 @@ def updateItem(request):
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
-    print(action)
-    print(productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, completed=False)
+
+    Ordered_Item, created = OrderedItem.objects.get_or_create(order=order, product=product)
+
+    if action == "add":
+        Ordered_Item.quantity = (Ordered_Item.quantity + 1)
+    elif action == 'remove':
+        Ordered_Item.quantity = (Ordered_Item.quantity - 1)
+
+    Ordered_Item.save()
+
+    if Ordered_Item.quantity <= 0:
+        Ordered_Item.delete()
+
     return JsonResponse("added the item", safe=False)
