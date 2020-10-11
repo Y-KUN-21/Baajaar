033db266afee0dcc76a3da306d2cd57947fe2bb6
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-10-11 20:30:10 +0530

    Add files via upload

--- baajaar_app/urls.py
+++ baajaar_app/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('checkout/', views.checkout, name='checkout'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('cart/', views.cart, name='cart'),
     path('update-item/', views.updateItem, name='update'),
     path('process-order/', views.processOrder, name='process_order')
